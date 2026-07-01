96d760336788669508c5fc8e82b7b81e6567bf35
MrMahirr <yunusemremahir121@gmail.com>
2026-07-01 14:06:37 +0300

    Initial commit

---
+++ src/main/java/dev/hashmark/hashmarkbackend/HashmarkBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.hashmark.hashmarkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashmarkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HashmarkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/hashmark/hashmarkbackend/HashmarkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hashmark.hashmarkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashmarkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
